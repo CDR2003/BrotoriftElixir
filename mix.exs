c4b663dab0e63da24a26abbf55ff549c8837a0ad
Peter Ren <lcdcdr2004@gmail.com>
2018-06-12 15:52:15 +0800

    0.2.1: {:active, true} -> {:active, false}, Added data_head

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Brotorift.MixProject do
   def project do
     [
       app: :brotorift,
-      version: "0.1.2",
+      version: "0.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       package: package(),
